52fee9e2f9b8689ee54963c5ce500cf1c96a6c41
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-07-01 11:05:16 +0530

    Update the game over sceane with restart btn

---
+++ assets/Scripts/RestartButton.ts
@@ -0,0 +1,18 @@
+import { _decorator, Component, Button, director } from "cc";
+const { ccclass } = _decorator;
+
+@ccclass("RestartButton")
+export class RestartButton extends Component {
+  start() {
+    // Optionally, you can set up the click event in code:
+    const button = this.getComponent(Button);
+    if (button) {
+      button.node.on(Button.EventType.CLICK, this.onPlayClicked, this);
+    }
+  }
+
+  onPlayClicked() {
+    // Replace 'Game' with the exact name of your game scene asset (case-sensitive)
+    director.loadScene("Game");
+  }
+}
